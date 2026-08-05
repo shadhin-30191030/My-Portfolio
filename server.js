48199b5c8af80901fd7fa6b1a416af2a57fbc9ae
Sabbir <sabbirahmmed2603@gmail.com>
2026-08-05 12:18:35 +0600

    first commit

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.static(__dirname));
 
-// Send index.html for the root route
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Local server
 app.listen(PORT, () => {
   console.log(`Portfolio running at http://localhost:${PORT}`);
 });
 
-// Export the Express API for Vercel
 module.exports = app;
